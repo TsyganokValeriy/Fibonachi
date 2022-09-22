dc792cac676f131f2de2387db1ae5b7b1e0b2db9
TsyganokValeriy <v.tsyganok@knu.ua>
2022-09-22 16:16:14 +0300

    init commit

---
+++ Fibonachi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Fibonachi
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0;  
+            Console.Write( a + " " + b);  
+            for (int i = 2; i < 15; i++)  
+            {  
+                c= a + b;  
+                System.Console.Write(" " + c );  
+                a= b;  
+                b= c;  
+            }
+        }
+    }
+}
